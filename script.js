b3efad57497d7ef304ca7baabe743564735fe447
Master-Daniels <adebayooluwasegun011@gmail.com>
2022-06-24 11:49:13 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log(`
+    Name: Adebayo Daniel.
+    Height: 5feet 7inches.
+    Country: Nigerian.
+`);
+alert(`
+    Name: Adebayo Daniel.
+    Height: 5feet 7inches.
+    Country: Nigerian.
+`);
+document.write(`
+   <h1> Name: Adebayo Daniel.<h1>
+   <h2> Height: 5feet 7inches. <h2>
+   <h2> Country: Nigerian. <h2>
+`);
+
+// Added the three methods cos not sure wg=hich one is correct.
